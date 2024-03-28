71dd7a548d898c37cd1a5fed1597a44502b96e06
Roney Dsilva <roney.dsilva@cdmx.in>
2024-03-28 17:46:32 +0530

    commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# wappler_sc_s3_upload
+```
+# AWS S3 Signed Upload
+
+## Overview
+This functionality enables the generation of signed URLs for uploading files to an Amazon S3 bucket securely. Signed URLs are URLs for objects in S3 that are valid for a limited time and can be used to upload files directly to S3 without requiring the uploader to have AWS credentials.
+
+## Usage
+### S3 Details
+- **Bucket**: Enter the name of the S3 bucket where the file will be uploaded.
+- **Key**: Enter the key for the object in S3.
+- **Expires In (seconds)**: Enter the expiration time for the signed URL in seconds. Defaults to 300 seconds (5 minutes).
+- **ACL**: Enter the Access Control List (ACL) for the object. If not provided, ACL will not be set.
+
+### AWS Credentials
+- **Access Key ID**: Enter the AWS access key ID.
+- **Secret Access Key**: Enter the AWS secret access key.
+- **Region**: Enter the AWS region. Defaults to 'us-east-1'.
+```
---
+++ awsS3Signed.js
@@ -0,0 +1,21 @@
+const mime = require('mime-types');
+const { S3 } = require('@aws-sdk/client-s3');
+
+exports.aws_s3_signed_upload = async function (options) {
+    const Bucket = this.parseRequired(options.bucket, 'string', 'Bucket is required.');
+    const Key = this.parseRequired(options.key, 'string', 'Key is required.');
+    const ContentType = this.parseOptional(options.contentType, 'string', mime.lookup(Key) || 'application/octet-stream');
+    const expiresIn = this.parseOptional(options.expires, 'number', 300);
+    const ACL = this.parseOptional(options.acl, 'string', undefined);
+    const accessKeyId = this.parseRequired(options.accessKeyId, 'string', 'AccessKeyId is required.');
+    const secretAccessKey = this.parseRequired(options.secretAccessKey, 'string', 'SecretAccessKey is required.');
+    const region = this.parseOptional(options.region, 'string', 'us-east-1');
+    const endpoint = `s3.${region}.amazonaws.com`;
+    const s3 = new S3({ endpoint: 'https://' + endpoint, credentials: { accessKeyId, secretAccessKey }, region, signatureVersion: 'v4' });
+
+    const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+    const { PutObjectCommand } = require('@aws-sdk/client-s3');
+    const command = new PutObjectCommand({ Bucket, Key, ContentType, ACL });
+
+    return getSignedUrl(s3, command, { expiresIn });
+}
