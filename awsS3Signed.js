63ce4db790885ae5e839a00f56d7bf0fc15cfc0e
Roney Dsilva <roney.dsilva@cdmx.in>
2024-03-28 18:39:45 +0530

    updates

--- awsS3Signed.js
+++ awsS3Signed.js
@@ -1,5 +1,6 @@
-const mime = require('mime-types');
 const { S3 } = require('@aws-sdk/client-s3');
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { PutObjectCommand, GetObjectCommand } = require('@aws-sdk/client-s3');
 
 exports.aws_s3_signed_upload = async function (options) {
     const Bucket = this.parseRequired(options.bucket, 'string', 'Bucket is required.');
@@ -12,10 +13,19 @@ exports.aws_s3_signed_upload = async function (options) {
     const region = this.parseOptional(options.region, 'string', 'us-east-1');
     const endpoint = `s3.${region}.amazonaws.com`;
     const s3 = new S3({ endpoint: 'https://' + endpoint, credentials: { accessKeyId, secretAccessKey }, region, signatureVersion: 'v4' });
-
-    const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
-    const { PutObjectCommand } = require('@aws-sdk/client-s3');
     const command = new PutObjectCommand({ Bucket, Key, ContentType, ACL });
+    return getSignedUrl(s3, command, { expiresIn });
+}
 
+exports.aws_s3_signed_download = async function (options) {
+    const Bucket = this.parseRequired(options.bucket, 'string', 'Bucket is required.');
+    const Key = this.parseRequired(options.key, 'string', 'Key is required.');
+    const expiresIn = this.parseOptional(options.expires, 'number', 300);
+    const accessKeyId = this.parseRequired(options.accessKeyId, 'string', 'AccessKeyId is required.');
+    const secretAccessKey = this.parseRequired(options.secretAccessKey, 'string', 'SecretAccessKey is required.');
+    const region = this.parseOptional(options.region, 'string', 'us-east-1');
+    const endpoint = `s3.${region}.amazonaws.com`;
+    const s3 = new S3({ endpoint: 'https://' + endpoint, credentials: { accessKeyId, secretAccessKey }, region, signatureVersion: 'v4' });
+    const command = new GetObjectCommand({ Bucket, Key });
     return getSignedUrl(s3, command, { expiresIn });
 }
